626895d911e5a98bb9b86b602c404b474dc4a0ca
BERNIGAUD SAMATAN Lucas <l.bernigaudsamatan@arvalis.fr>
2022-11-05 16:16:12 +0100

    write methods to class chess_board to communicate with stockfish engine

---
+++ chessai_vs_stockfish.py
@@ -0,0 +1,34 @@
+from Class_board import chess_board
+from stockfish_utils import pgn2fen
+from stockfish import Stockfish
+import random as rd
+
+
+chessboard = chess_board()
+chessboard.initialize()
+stockfish_engine = Stockfish("/home/lucas/Documents/Lucas/Python_projects/stockfish_15_linux_x64/stockfish_15_x64",depth=1)
+chessboard.readuci("e8c8")
+chessboard.readuci("g7h8q")
+end = False
+while not end:
+
+    if chessboard.white_moves() == []:
+        end = True
+        continue
+
+    white_move = rd.choice(chessboard.white_moves())
+    chessboard.update_pgn(chessboard.move_to_pgn(white_move))
+    chessboard.play_move(white_move)
+
+    if chessboard.black_moves() == []:
+        end = True
+
+    fen = pgn2fen(chessboard.pgn_string)
+    stockfish_engine.set_fen_position(fen)
+
+    black_move = stockfish_engine.get_best_move()
+    black_move = chessboard.readuci(black_move)
+    chessboard.update_pgn(chessboard.move_to_pgn(black_move))
+    chessboard.play_move(black_move)
+
+print(chessboard.pgn_string)
---
+++ stockfish_utils.py
@@ -0,0 +1,17 @@
+import stockfish
+import chess
+import chess.pgn
+from Class_board import chess_board
+from io import StringIO
+from typing import AnyStr
+
+
+def pgn2fen(pgn_string: AnyStr) -> AnyStr:
+    """
+    take a pgn string and return a fen string
+    """
+    game = chess.pgn.read_game(StringIO(pgn_string))
+    board = game.board()
+    for m in game.mainline_moves():
+        board.push(m)
+    return board.fen()
